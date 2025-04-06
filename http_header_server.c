fc575654823c49db54e72e0d8b4042b32d9f390d
max <maxr.0307@gmail.com>
2025-04-07 00:41:14 +0200

    Got tcp working

--- http_header_server.c
+++ http_header_server.c
@@ -6,10 +6,10 @@
 
 #define PORT "8814"
 #define BACKLOG 5
+#define BUFFER_SIZE 65507
 
 int main() {
   struct addrinfo hints, *res;
-  int s;
 
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_UNSPEC;
@@ -21,12 +21,27 @@ int main() {
     return 1;
   }
 
-  s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  int s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
   if (bind(s, res->ai_addr, res->ai_addrlen) < 0) {
     perror("bind() error:");
     return 1;
   }
 
-  listen(s, BACKLOG);
+  if (listen(s, BACKLOG) < 0) {
+    perror("listen() error:");
+    return 1;
+  }
+
+  struct sockaddr_storage their_addr;
+  socklen_t their_addr_size = sizeof(their_addr);
+  int new_socket = accept(s, (struct sockaddr *)&their_addr, &their_addr_size);
+
+  char buf[BUFFER_SIZE];
+  if (recv(new_socket, buf, BUFFER_SIZE, 0) < 0) {
+    perror("recv() error:");
+    return 1;
+  }
+
+  printf("%s\n", buf);
 }
